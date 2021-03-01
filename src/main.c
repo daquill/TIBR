059ed7dd2617dd46e2390f823fa6c80fc25c1f49
King Dub Dub <50710395+KingDubDub@users.noreply.github.com>
2021-02-28 19:56:58 -0500

    Cleaned up main code style as an example
    My eyes no longer bleed, and it should be easier for you to debug.

--- src/main.c
+++ src/main.c
@@ -22,43 +22,45 @@
 void main(void)
 {
    bool signinstatus = true;
-    bool allowgamekp = true;
-    int playerx = 50;
-    int playery = 50;
-    bool gamestatus = false;
+   bool allowgamekp = true;
+   int playerx = 50;
+   int playery = 50;
+   bool gamestatus = false;
    
    gfx_Begin();
-    gfx_SetDrawBuffer();
+   gfx_SetDrawBuffer();
    
-    do{
-       
-        for (uint24_t filltilex = 0; filltilex < 320; filltilex+=16) {
-            for (uint8_t filltiley = 0; filltiley < 240; filltiley) {
-             gfx_Sprite(stonebricktiletibr,filltilex,filltiley);
+   do{    
+        for (uint24_t filltilex = 0; filltilex < 320; filltilex+=16)
+        {
+            for (uint8_t filltiley = 0; filltiley < 240; filltiley)
+            {
+               gfx_Sprite(stonebricktiletibr,filltilex,filltiley);
             }    
         }
    
-        if (gamestatus) {
-         gfx_Sprite(PlayerAvatar,playerx,playery);
-         gfx_FillRectangle(0,0,10,10);
-            if (allowgamekp) {
-            kb_Scan();
-            if (kb_Data[7] & kb_Up)
-               playery = playery++;
-           
-            if (kb_Data[7] & kb_Down)
-               playery = playery--;
-               
-            if (kb_Data[7] & kb_Left)
-               playerx = playerx--;
-               
-            if (kb_Data[7] & kb_Right)
-               playerx = playerx++;
-            }
+        if (gamestatus)
+        {
+           gfx_Sprite(PlayerAvatar,playerx,playery);
 
+           gfx_FillRectangle(0,0,10,10);
+
+           if (allowgamekp)
+           {
+               kb_Scan();
+
+               if (kb_Data[7] & kb_Up)   playery = playery++;
+
+               if (kb_Data[7] & kb_Down) playery = playery--;
+
+               if (kb_Data[7] & kb_Left) playerx = playerx--;
+
+               if (kb_Data[7] & kb_Right) playerx = playerx++;
+           }
         }
 
         gfx_FillScreen(19);
+      
         gfx_SetTextFGColor(228);
         gfx_PrintStringXY("Battle Royale",140,100);
         gfx_SetTextFGColor(187);
@@ -68,15 +70,15 @@ void main(void)
      
       if (signinstatus == 1) signinstatus = 2;
      
-      if(kb_Data[1] && kb_2nd && signinstatus == 0) {
+      if(kb_Data[1] && kb_2nd && signinstatus == 0)
+      {
          gfx_SetTextFGColor(147);
          signinstatus = 1;
       }
      
-        gfx_BlitBuffer();
-   
-    } while(!(kb_Data[6] & kb_Clear));
-   
+      gfx_BlitBuffer();
+
+   } while(!(kb_Data[6] & kb_Clear));
 
     gfx_End();
 }
