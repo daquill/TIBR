eb5e4150e30952a579d2cff5c1d10fb3b8734b3d
QuillPlayzYT <nicknasia123@gmail.com>
2021-03-04 11:38:39 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,6 +10,18 @@
  *--------------------------------------
 */
 
+//Thanks to Alvajoy on cemetech for the new and furnished code of a alpha release
+//This New Build was made with the TI Project Builder
+
+/*
+ *--------------------------------------
+ * Program Name:BATROYAL
+ * Author: QuillPlayzyT
+ * License: -
+ * Description: Battle Royale for the TI 84 Plus CE
+ *--------------------------------------
+*/
+
 #include <tice.h>
 #include <graphx.h>
 #include <keypadc.h>
@@ -17,68 +29,71 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
-#include "gfx/gfx.h"
+// Remove this line once in VS Code or preferred IDE Editor #include "stonebricktiletibr.c"
+// Remove this line once in VS Code or preferred IDE Editor #include "stonebricktiletibr.h"
+// Add below line once in VS Code : I had to remove for that way it could compile
+// #include "gfx/gfx.h"
 
-void main(void)
+void signinscreen()
 {
-   bool signinstatus = true;
-   bool allowgamekp = true;
-   int playerx = 50;
-   int playery = 50;
-   bool gamestatus = false;
-   
-   gfx_Begin();
-   gfx_SetDrawBuffer();
-   
-   do{    
+    bool signinstatus = true;
+
+
+
+    gfx_Begin();
+    gfx_SetDrawBuffer();
+
+    do {
         for (uint24_t filltilex = 0; filltilex < 320; filltilex+=16)
         {
-            for (uint8_t filltiley = 0; filltiley < 240; filltiley)
+            for (uint8_t filltiley = 0; filltiley < 240; filltiley++)
             {
-               gfx_Sprite(stonebricktiletibr,filltilex,filltiley);
-            }    
-        }
-   
-        if (gamestatus)
-        {
-           gfx_Sprite(PlayerAvatar,playerx,playery);
-
-           gfx_FillRectangle(0,0,10,10);
-
-           if (allowgamekp)
-           {
-               kb_Scan();
-
-               if (kb_Data[7] & kb_Up)   playery = playery++;
-
-               if (kb_Data[7] & kb_Down) playery = playery--;
-
-               if (kb_Data[7] & kb_Left) playerx = playerx--;
-
-               if (kb_Data[7] & kb_Right) playerx = playerx++;
-           }
+                gfx_Sprite(stonebricktiletibr,filltilex,filltiley);
+            }
         }
 
-        gfx_FillScreen(19);
-      
+
         gfx_SetTextFGColor(228);
-        gfx_PrintStringXY("Battle Royale",140,100);
+        gfx_PrintStringXY("Battle Royale",120,100);
         gfx_SetTextFGColor(187);
-        gfx_PrintStringXY("Pre Release 1.0",136,110);
+        gfx_PrintStringXY("Pre Release 1.0",116,110);
         gfx_SetTextFGColor(147);
-        gfx_PrintStringXY("2nd To Login with your TIBR account ",70,120);
-     
-      if (signinstatus == 1) signinstatus = 2;
-     
-      if(kb_Data[1] && kb_2nd && signinstatus == 0)
-      {
-         gfx_SetTextFGColor(147);
-         signinstatus = 1;
-      }
-     
-      gfx_BlitBuffer();
+        gfx_PrintStringXY("2nd To Login with your TIBR account ",40,120);
 
-   } while(!(kb_Data[6] & kb_Clear));
+        if (signinstatus == 1) signinstatus = 2;
+
+        if(kb_Data[1] & kb_2nd && signinstatus == 0)
+        {
+            gfx_SetTextFGColor(147);
+            signinstatus = 1;
+        }
+
+        gfx_BlitBuffer();
+
+    } while(!(kb_Data[6] & kb_Clear));
 
     gfx_End();
+}
+void thegame(void)
+{
+    bool allowgamekp = true;
+    int playerx = 50;
+    int playery = 50;
+ do {
+    
+    if (allowgamekp)
+            {
+                kb_Scan();
+
+                if (kb_Data[7] & kb_Up)   playery++;
+
+                if (kb_Data[7] & kb_Down) playery--;
+
+                if (kb_Data[7] & kb_Left) playerx--;
+
+                if (kb_Data[7] & kb_Right) playerx++;
+            }
+
+ }
+    
 }
